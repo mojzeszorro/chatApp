c8268925dbd94f53729bc3b306c44fa840d4c7a6
mojzeszorro <44746319+mojzeszorro@users.noreply.github.com>
2019-02-17 23:45:34 +0100

    js stuff

--- app.py
+++ app.py
@@ -6,8 +6,13 @@ app=Flask(__name__)
 app.config["SECRET_KEY"] = "VERYNOTSECRETKEY"
 socketio = SocketIO(app)
 
-@app.route("/")
+@app.route("/", methods=["GET","POST"])
 def index():
     return render_template("index.html")
+@socketio.on('message')
+def send(msg):
+  msg=request.form.get('username')
+  emit("send_msg", {"msg":msg},broadcast=True)
+
 if __name__ == '__main__':
   socketio.run(app)
--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,21 @@
+document.addEventListener('DOM ContentLoaded', function() {
+  var socket=io.connect(location.protocol +'//'+document.domain + ':' + location.port);
+  socket.on('connect', function(){
+    
+    document.querySelector('#username').onsubmit = function() {
+      const user=document.querySelector("#username").value;
+      socket.emit('message',{'user':user});
+      
+  };
+});
+
+socket.on('send_msg', data => {
+  const li=document.createElement('li');
+  li.innerHTML='msg:${data.user}';
+  document.querySelector("#messages").append(li);
+});
+});
+
 var el=document.querySelector("#channel-list")
 function openNav() {
     
@@ -7,3 +25,15 @@ function openNav() {
   function closeNav() {
     el.style.width = "10%";
   }
+  $(function() {
+    $('form').each(function() {
+        $(this).find('input').keypress(function(e) {
+            // Enter pressed?
+            if(e.which == 10 || e.which == 13) {
+                this.form.submit();
+            }
+        });
+
+        $(this).find('input[type=submit]').hide();
+    });
+});
